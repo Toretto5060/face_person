500cac6d6189adc03e23f973dc6f3dd05ee39844
zhanglei <zhanglei@inesadt.com>
2019-05-17 18:22:20 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="home">
+    <div class="contenr">
+      <div class="videocon">
+        <video  ref="video" width="200" height="150px" autoplay></video>
+      </div>
+      <div class="btncon">
+        <input type="button"  style="width: 100px;height: 35px" value="开启认证" @click="getVideo">
+        <input type="button"  style="width: 100px;height: 35px;" value="截取" @click="takePhoto">
+        <input type="button" ref="bg"  style="width: 100px;height: 35px;" value="开始采集" @click="off">
+        <input type="button" ref="close" style="width: 100px;height: 35px;" value="停止采集" @click="on">
+        <a  ref="save" style="display:block;width: 100px;height: 35px;text-align: center;line-height: 35px">保存到本地</a>
+        <span>{{videolength + "s"}}</span>
+      </div>
+      <div class="cancon">
+        <canvas ref="canvas" width="200" height="150"></canvas>
+      </div>
+      <div id=""content></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      mediaRecorder: "",
+      videolength: 0,
+      timer: null
+    };
+  },
+  mounted() {},
+  methods: {
+    // 开启摄像头
+    getVideo() {
+        var video = this.$refs.video;
+        var videoObj = {
+          video: true,
+          audio: true
+        }
+        let self = this;
+        navigator.mediaDevices
+          .getUserMedia(videoObj)
+          .then(function (mediaStream) {
+            video.srcObject = mediaStream
+            video.play()
+            self.mediaRecorder = new MediaRecorder(mediaStream, {
+              audioBitsPerSecond : 128000,  // 音频码率
+              videoBitsPerSecond : 100000,  // 视频码率
+              mimeType : 'video/webm;codecs=h264' // 编码格式
+            })
+          })
+          .catch(function (error) {
+            console.log(error)
+          });
+    },
+    // 抓拍人脸
+    takePhoto() {
+      let canvasCon =  this.$refs.canvas
+      let context = canvasCon.getContext("2d");
+      context.drawImage(this.$refs.video, 0, 0, 200, 150);
+      let imgSrc = canvasCon.toDataURL("image/png");
+      console.log(imgSrc)
+    },
+    //开始采集
+    off () {
+      this.mediaRecorder.start()
+      console.log('采集成功')
+      this.timer = setInterval(() => {
+        this.videolength += 1
+      },1000)
+    },
+    // 停止采集
+    on () {
+      this.mediaRecorder.stop()
+      console.log('停止采集');
+      clearInterval(this.timer);
+      this.timer = null
+      var a = this.$refs.save
+      this.mediaRecorder.ondataavailable = function (e) {
+        // console.log(e.data)
+        // // 下载视频
+        // var encodedUri = URL.createObjectURL(e.data)
+
+
+        var aFileParts = ['<a id="a"><b id="b">hey!</b></a>']; // 一个包含DOMString的数组
+        var text = new Blob(aFileParts, {type : 'text/html'}); // 得到 blob
+        var encodedUri = URL.createObjectURL(text)
+        a.href = encodedUri
+        // a.download = `test.mp4`
+        a.download = `test.txt`
+        console.log(encodedUri)
+      }
+    }
+  }
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+
+  // 将构建好的文件输出到哪里
+  outputDir: process.env.NODE_ENV === 'production' ? 'dist_pro' : 'dist_dev',
+
+  devServer: {
+    // host: 'localhost',
+    port: 6060,
+    open:false,
+    before: app => { }
+  },
+
+  lintOnSave: false
+}
